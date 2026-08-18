4ab524ac5bef91cd40c0b2ee22e47f0b882fefc0
wyp12345uu <wyp999001@gmail.com>
2026-08-18 15:19:37 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     response_text = f"🎬 {movie_name} ဇာတ်ကားအတွက် Review တောင်းဆိုမှုကို လက်ခံရရှိပါပြီ။"
     await update.message.reply_text(response_text, parse_mode="Markdown")
 
-if name == "__main__":
+if __name__ == "__main__":
     TOKEN = os.getenv("BOT_TOKEN")
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
